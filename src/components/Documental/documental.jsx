6021fa9118d29ff4c45c1da4a0df6d325b783c6c
ajavieralejandro <amorosijavier@gmail.com>
2023-07-17 19:49:21 -0300

    changes...

--- src/components/Documental/documental.jsx
+++ src/components/Documental/documental.jsx
@@ -4,42 +4,30 @@ const Documental = () =>{
     return(
         <>
         
-<div class="container my-24 mx-auto md:px-6">
+<div class="container my-24 mx-auto md:px-6 relative  ">
   <section class="mb-32">
     <div
       class="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
       <div class="flex flex-wrap items-center">
         <div class="hidden shrink-0 grow-0 basis-auto lg:flex lg:w-6/12 xl:w-4/12">
-          <img src="https://imagedelivery.net/rXQkQjLMcsIgr9J-xeKCWA/23d04424-fc57-413d-6d9a-64203471db00/public" alt="Trendy Pants and Shoes"
+          <img src="https://imagedelivery.net/rXQkQjLMcsIgr9J-xeKCWA/4e8ec02e-a823-4334-f20c-214b0ebcfd00/public" alt="Trendy Pants and Shoes"
             class="w-full rounded-t-lg lg:rounded-tr-none lg:rounded-bl-lg" />
         </div>
         <div class="w-full shrink-0 grow-0 basis-auto lg:w-6/12 xl:w-8/12">
           <div class="px-6 py-12 md:px-12">
             <h2 class="mb-4 text-2xl font-bold">
-              What's the secret of the great taste?
+              Tocar3d
             </h2>
-            <p class="mb-6 flex items-center font-bold uppercase text-danger dark:text-danger-500">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
-                stroke="currentColor" class="mr-2 h-5 w-5">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                  d="M15.362 5.214A8.252 8.252 0 0112 21 8.25 8.25 0 016.038 7.048 8.287 8.287 0 009 9.6a8.983 8.983 0 013.361-6.867 8.21 8.21 0 003 2.48z" />
-                <path stroke-linecap="round" stroke-linejoin="round"
-                  d="M12 18a3.75 3.75 0 00.495-7.467 5.99 5.99 0 00-1.925 3.546 5.974 5.974 0 01-2.133-1A3.75 3.75 0 0012 18z" />
-              </svg>
-              Hot news
-            </p>
+ 
             <p class="mb-6 text-neutral-500 dark:text-neutral-300">
             TOCAR es un proyecto especial de Ágora 3D Academy y Agora 3D Studio premiado por el Ministerio de Cultura de la Nación Argentina y cuenta con el apoyo de diferentes empresas con una visión en común: ampliar posibilidades y mejorar al mundo. El objetivo del proyecto es dar acceso a la población de personas ciegas a piezas del arte y el patrimonio cultural, mediante la digitalización de las mismas para su posterior impresión 3D a través de software y hardware específicamente diseñados para cumplir con la tarea. De ésta labor se desprende la posibilidad de generar una biblioteca virtual de los bienes digitalizados para las instituciones con las que el proyecto se lleve a cabo, y proveer a museos, instituciones educativas, investigadores, estudiantes y hobbistas de un programa y modelo de creación de redes, laboratorios y dispositivos inclusivos.
             </p>
             <p class="text-neutral-500 dark:text-neutral-300">
-              Commodi ut nisi assumenda alias maxime necessitatibus ad rem
-              repellat explicabo, reiciendis illum suscipit iusto? Provident
-              dignissimos similique, reiciendis inventore accusantium unde
-              mollitia, deleniti quae atque error id perspiciatis illum.
-              Laboriosam aperiam ab illo dignissimos obcaecati corporis
-              similique a odio, optio iste quis placeat alias amet rerum
-              sint quos dolor pariatur inventore possimus ad consequuntur
-              fugiat perferendis consectetur laudantium.
+            El proyecto Tocar busca acercar la arquitectura a las personas ciegas mediante impresiones hápticas de edificios,
+para que las personas ciegas puedan disfrutar de la arquitectura.
+Queremos replicar en otros lugares el proyecto. Creamos una plantilla para enseñar a otras personas  a realizar  las impresiones hápticas.
+El uso de blockchain permitiría mantener un registro seguro de las impresiones realizadas y fomentaría colaboraciones y contribuciones de personas interesadas en el proyecto.
+El conocimiento compartido es fundamental en el proyecto Tocar, ya que se busca crear una red global de apoyo y conocimiento en torno a la impresión háptica de edificios.
             </p>
           </div>
         </div>
--- src/components/Features/features.jsx
+++ src/components/Features/features.jsx
@@ -6,48 +6,48 @@ const Features = () =>{
         <section class="text-gray-600 bg-white body-font">
   <div class="container px-5 py-24 mx-auto">
     <div class="flex flex-col text-center w-full mb-20">
-      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Master Cleanse Reliac Heirloom</h1>
-      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
+      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Proyecto</h1>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Algunos de nuestros logros que fueron posible gracias a los coloboradores e instituciones que acompañan nuestro proyecto.</p>
     </div>
     <div class="flex flex-wrap -m-4 text-center">
       <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
         <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="text-indigo-500 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
-            <path d="M8 17l4 4 4-4m-4-5v9"></path>
-            <path d="M20.88 18.09A5 5 0 0018 9h-1.26A8 8 0 103 16.29"></path>
-          </svg>
-          <h2 class="title-font font-medium text-3xl text-gray-900">2.7K</h2>
-          <p class="leading-relaxed">Downloads</p>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="text-indigo-500 w-12 h-12 mb-3 inline-block">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0012 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75z" />
+</svg>
+
+          <h2 class="title-font font-medium text-3xl text-gray-900">10</h2>
+          <p class="leading-relaxed">Edificios Escaneados</p>
         </div>
       </div>
       <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
         <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="text-indigo-500 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
-            <path d="M17 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2"></path>
-            <circle cx="9" cy="7" r="4"></circle>
-            <path d="M23 21v-2a4 4 0 00-3-3.87m-4-12a4 4 0 010 7.75"></path>
-          </svg>
-          <h2 class="title-font font-medium text-3xl text-gray-900">1.3K</h2>
-          <p class="leading-relaxed">Users</p>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="text-indigo-500 w-12 h-12 mb-3 inline-block">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z" />
+</svg>
+
+          <h2 class="title-font font-medium text-3xl text-gray-900">3</h2>
+          <p class="leading-relaxed">ciudades</p>
         </div>
       </div>
       <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
         <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="text-indigo-500 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
-            <path d="M3 18v-6a9 9 0 0118 0v6"></path>
-            <path d="M21 19a2 2 0 01-2 2h-1a2 2 0 01-2-2v-3a2 2 0 012-2h3zM3 19a2 2 0 002 2h1a2 2 0 002-2v-3a2 2 0 00-2-2H3z"></path>
-          </svg>
-          <h2 class="title-font font-medium text-3xl text-gray-900">74</h2>
-          <p class="leading-relaxed">Files</p>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="text-indigo-500 w-12 h-12 mb-3 inline-block">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+</svg>
+
+          <h2 class="title-font font-medium text-3xl text-gray-900">27</h2>
+          <p class="leading-relaxed">Colaboradores</p>
         </div>
       </div>
       <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
         <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="text-indigo-500 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
-            <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
-          </svg>
-          <h2 class="title-font font-medium text-3xl text-gray-900">46</h2>
-          <p class="leading-relaxed">Places</p>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="text-indigo-500 w-12 h-12 mb-3 inline-block">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819" />
+</svg>
+
+          <h2 class="title-font font-medium text-3xl text-gray-900">12</h2>
+          <p class="leading-relaxed">Monumentos históricos</p>
         </div>
       </div>
     </div>
