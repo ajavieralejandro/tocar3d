0a20ca4da98b3747a6ff595050a4dc8020aa8fc9
ajavieralejandro <amorosijavier@gmail.com>
2023-08-06 14:28:10 -0300

    change size of image

--- src/components/Institutions/institutions.jsx
+++ src/components/Institutions/institutions.jsx
@@ -3,7 +3,7 @@ const Institutions = () =>{
     return(
         <div className="bg-white">
             
-            <img className="mx-auto h-92" src="https://imagedelivery.net/rXQkQjLMcsIgr9J-xeKCWA/60b0f32b-e039-4e87-9721-9059917c3e00/public" alt="Logo Fundación Amigos del Palacio Barolo" />
+            <img className="mx-auto  h-72" src="https://imagedelivery.net/rXQkQjLMcsIgr9J-xeKCWA/60b0f32b-e039-4e87-9721-9059917c3e00/public" alt="Logo Fundación Amigos del Palacio Barolo" />
         </div>
     )
 }
