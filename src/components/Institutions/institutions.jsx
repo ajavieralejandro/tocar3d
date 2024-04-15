17c0a3e7e5928475805748553c7226e09bedac00
ajavieralejandro <amorosijavier@gmail.com>
2024-04-15 11:04:25 -0300

    team changes etc...

--- src/components/Institutions/institutions.jsx
+++ src/components/Institutions/institutions.jsx
@@ -3,13 +3,17 @@ const Institutions = () =>{
     return(
         <div className="bg-white pb-12">
                     <h2 class="mb-8 lg:mb-16 text-3xl font-extrabold tracking-tight leading-tight text-center text-gray-900 dark:text-white md:text-4xl">Instituciones que nos apoyan:</h2>
-                    <div class="grid sm:grid-cols-1 md:grid-cols-2 ">
+                    <div class="grid sm:grid-cols-1 md:grid-cols-3 ">
                         <div>
                         <img className="mx-auto  h-72" src="https://imagedelivery.net/rXQkQjLMcsIgr9J-xeKCWA/60b0f32b-e039-4e87-9721-9059917c3e00/public" alt="Logo Fundación Amigos del Palacio Barolo" />
 
                         </div>
                         <div>
                         <img className="mx-auto  h-72" src="https://imagedelivery.net/rXQkQjLMcsIgr9J-xeKCWA/ea11548c-c071-44a1-9956-35596f3a4e00/public" alt="Logo Kernel" />
+
+                        </div>
+                        <div>
+                        <img className="mx-auto  h-72" src="https://imagedelivery.net/rXQkQjLMcsIgr9J-xeKCWA/1675e947-f8bc-4ea1-84c5-9bd0407dcf00/public" alt="Logo BAC" />
 
                         </div>
                     </div>
