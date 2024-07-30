192af9608db26accf76bc158f8b92c7cda79ca03
ajavieralejandro <amorosijavier@gmail.com>
2024-07-30 18:30:09 -0300

    changes on logos

--- src/components/Institutions/institutions.jsx
+++ src/components/Institutions/institutions.jsx
@@ -1,21 +1,38 @@
 import React from "react";
 const Institutions = () =>{
     return(
-        <div className="bg-white pb-12">
+        <div className="pb-12 bg-white">
                     <h2 class="mb-8 lg:mb-16 text-3xl font-extrabold tracking-tight leading-tight text-center text-gray-900 dark:text-white md:text-4xl">Instituciones que nos apoyan:</h2>
                     <div class="grid sm:grid-cols-1 md:grid-cols-3 ">
                         <div>
-                        <img className="mx-auto  h-72" src="https://imagedelivery.net/rXQkQjLMcsIgr9J-xeKCWA/60b0f32b-e039-4e87-9721-9059917c3e00/public" alt="Logo Fundación Amigos del Palacio Barolo" />
+                        <img className="mx-auto h-72" src="https://imagedelivery.net/rXQkQjLMcsIgr9J-xeKCWA/60b0f32b-e039-4e87-9721-9059917c3e00/public" alt="Logo Fundación Amigos del Palacio Barolo" />
 
                         </div>
                         <div>
-                        <img className="mx-auto  h-72" src="https://imagedelivery.net/rXQkQjLMcsIgr9J-xeKCWA/ea11548c-c071-44a1-9956-35596f3a4e00/public" alt="Logo Kernel" />
+                        <img className="mx-auto h-72" src="https://imagedelivery.net/rXQkQjLMcsIgr9J-xeKCWA/ea11548c-c071-44a1-9956-35596f3a4e00/public" alt="Logo Kernel" />
 
                         </div>
                         <div>
-                        <img className="mx-auto  h-72" src="https://imagedelivery.net/rXQkQjLMcsIgr9J-xeKCWA/1675e947-f8bc-4ea1-84c5-9bd0407dcf00/public" alt="Logo BAC" />
+                        <img className="mx-auto h-72" src="https://imagedelivery.net/rXQkQjLMcsIgr9J-xeKCWA/1675e947-f8bc-4ea1-84c5-9bd0407dcf00/public" alt="Logo BAC" />
 
                         </div>
+
+
+                        <div>
+                        <img className="mx-auto h-72" src="https://imagedelivery.net/rXQkQjLMcsIgr9J-xeKCWA/c6681d1e-bdb5-42c1-e1bb-2f660a12de00/public" alt="Logo chimak" />
+
+                        </div>
+
+                        <div>
+                        <img className="mx-auto h-72" src="https://imagedelivery.net/rXQkQjLMcsIgr9J-xeKCWA/b3d2920b-b145-4f5e-6f0c-987daf53c800/public" alt="Logo 3d insumos" />
+
+                        </div>
+
+                        <div>
+                        <img className="mx-auto h-72" src="https://imagedelivery.net/rXQkQjLMcsIgr9J-xeKCWA/f5d339f0-f93a-41b5-c164-4b572baa0400/public" alt="Logo Marker Parts" />
+
+                        </div>
+
                     </div>
      
         </div>
